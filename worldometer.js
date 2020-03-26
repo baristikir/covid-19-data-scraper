651d249d8555f33ad965ec71c7cedc8819a98f63
Baris Tikir <brstkr@icloud.com>
2020-03-26 10:15:43 +0100

    Added Scraping Total Cases Functionality

--- worldometer.js
+++ worldometer.js
@@ -1,7 +1,16 @@
 const puppeteer = require('puppeteer-extra')
 
 puppeteer.launch({
-    headless: false
+    headless: true
 }).then(async browser => {
-    
+    const page = await browser.newPage();
+
+    await page.goto('https://www.worldometers.info/coronavirus');
+
+    page.waitForSelector('.maincounter-number')
+    .then(async function() {
+        const totalCases = await page.$eval('.maincounter-number span', element => element.innerHTML);
+        console.log('Total Cases::', totalCases); 
+    })
+
 })
